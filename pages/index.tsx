60f694c10d2d470c146179560c4d0ff8e3bcfa1f
Rafael <rafael@MacBook-Pro-de-Rafael.local>
2021-02-21 11:56:47 -0500

    Adicionando configuração inicial

---
+++ pages/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Head from 'next/head'
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>
+          Hello World
+        </h1>
+      </main>
+    </div>
+  )
+}
+
+export default Home
